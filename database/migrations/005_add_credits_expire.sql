4ce460096ae8db07ccf5eb85e80cc50cb208eb19
alkor2000 <200923177@qq.com>
2025-07-25 02:11:11 +0800

    移除旧的迁移文件

--- database/migrations/005_add_credits_expire.sql
+++
@@ -1,8 +0,0 @@
--- 添加积分有效期字段
-ALTER TABLE users ADD COLUMN credits_expire_at TIMESTAMP NULL DEFAULT NULL COMMENT '积分过期时间' AFTER used_credits;
-
--- 为现有用户设置默认过期时间（365天后）
-UPDATE users SET credits_expire_at = DATE_ADD(NOW(), INTERVAL 365 DAY) WHERE credits_expire_at IS NULL AND credits_quota > 0;
-
--- 添加索引以优化查询
-ALTER TABLE users ADD INDEX idx_credits_expire (credits_expire_at);
